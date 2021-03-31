46de7b77affafae7527d769065d260bc16469492
mathieuLU <you@example.com>
2021-03-31 16:37:45 +0200

    config

---
+++ lib/config_template.php
@@ -0,0 +1,8 @@
+<?php
+
+define("PATH_PHP", "/usr/bin/php");
+
+define("PATH_WEBSERVER_ROOT", "/home/blabla/");
+define("PATH_CONVERT_DATA", PATH_WEBSERVER_ROOT."convert-SFT-SEBMS-to-MongoDB/");
+
+define ("URL_WEBSITE", "http://localhost/");
