745c1ea59973c5407b81b0a1824c94a23e87e6ef
mathieuLU <you@example.com>
2022-01-13 16:02:59 +0100

    add modification of latitude/longitude

--- process/activity_move_route_change_database.php
+++ process/activity_move_route_change_database.php
@@ -17,41 +17,55 @@ if ($okSite) {
 
     $consoleTxt.=consoleMessage("info", "Ready to change the data with $siteNewCorrectId");
 
+    if (!isset($siteNewDetails->geoIndex->coordinates[0]) || !is_numeric($siteNewDetails->geoIndex->coordinates[0])  || !isset($siteNewDetails->geoIndex->coordinates[1]) || !is_numeric($siteNewDetails->geoIndex->coordinates[1]) ) {
+        $consoleTxt.=consoleMessage("info", "No coordinates found in geoIndex.coordinates for the new site");
+    }
 
-    $bulkP = new MongoDB\Driver\BulkWrite;
-    //$filter = [];
-    $filter = ['activityId' => $activityIdToFix];
-    $options =  ['$set' => ['siteId' => $siteNewCorrectId]];
-    $updateOptions = [];
-    $bulkP->update($filter, $options, $updateOptions); 
-    $result = $mng->executeBulkWrite('ecodata.activity', $bulkP);
+    else {
 
-    $consoleTxt.=consoleMessage("info", $result->getModifiedCount()." activity updated");
+        $longitude=$siteNewDetails->geoIndex->coordinates[0];
+        $latitude=$siteNewDetails->geoIndex->coordinates[1];
 
+        $bulkP = new MongoDB\Driver\BulkWrite;
+        //$filter = [];
+        $filter = ['activityId' => $activityIdToFix];
+        $options =  ['$set' => ['siteId' => $siteNewCorrectId]];
+        $updateOptions = [];
+        $bulkP->update($filter, $options, $updateOptions); 
+        $result = $mng->executeBulkWrite('ecodata.activity', $bulkP);
 
-    $bulkP = new MongoDB\Driver\BulkWrite;
-    //$filter = [];
-    $filter = ['activityId' => $activityIdToFix];
-    $options =  ['$set' => ["data.location" => $siteNewCorrectId]];
-    $updateOptions = [];
-    $bulkP->update($filter, $options, $updateOptions); 
-    $result = $mng->executeBulkWrite('ecodata.output', $bulkP);
-
-    $consoleTxt.=consoleMessage("info", $result->getModifiedCount()." output updated");
-
-    $bulkP = new MongoDB\Driver\BulkWrite;
-    //$filter = [];
-    $filter = ['activityId' => $activityIdToFix];
-    $options =  ['$set' => ['locationID' => $siteNewCorrectId]];
-    $updateOptions = ['multi' => true];
-    $bulkP->update($filter, $options, $updateOptions); 
-    $result = $mng->executeBulkWrite('ecodata.record', $bulkP);
-
-    $consoleTxt.=consoleMessage("info", $result->getModifiedCount()." record(s) updated");
-    $finalOk=true;
+        $consoleTxt.=consoleMessage("info", $result->getModifiedCount()." activity updated");
 
 
-    include "process/activity_move_route_get_details.php";
+        $bulkP = new MongoDB\Driver\BulkWrite;
+        //$filter = [];
+        $filter = ['activityId' => $activityIdToFix];
+        $options =  ['$set' => ["data.location" => $siteNewCorrectId]];
+        $updateOptions = [];
+        $bulkP->update($filter, $options, $updateOptions); 
+        $result = $mng->executeBulkWrite('ecodata.output', $bulkP);
+
+        $consoleTxt.=consoleMessage("info", $result->getModifiedCount()." output updated");
+
+        $bulkP = new MongoDB\Driver\BulkWrite;
+        //$filter = [];
+        $filter = ['activityId' => $activityIdToFix];
+        $options =  ['$set' => [
+            'locationID' => $siteNewCorrectId,
+            'decimalLatitude' => $latitude,
+            'decimalLongitude' => $longitude,
+        ]];
+        $updateOptions = ['multi' => true];
+        $bulkP->update($filter, $options, $updateOptions); 
+        $result = $mng->executeBulkWrite('ecodata.record', $bulkP);
+
+        $consoleTxt.=consoleMessage("info", $result->getModifiedCount()." record(s) updated");
+        $finalOk=true;
+
+
+        include "process/activity_move_route_get_details.php";
+    }
+
 
 }
 else {
