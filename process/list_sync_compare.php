b69fdcebdf32666f20cfba6161c2c85484b2d3f3
mathieuLU <you@example.com>
2023-02-07 09:08:21 +0100

    add table to page check art

--- process/list_sync_compare.php
+++ process/list_sync_compare.php
@@ -12,11 +12,12 @@ $tabArtRef=array();
 // Get the eurolist table as base
 $db_connection = pg_connect("host=".$DB["host"]." dbname=".$DB["database"]." user=".$DB["username"]." password=".$DB["password"])  or die("CONNECT:" . consoleMessage("error", pg_result_error()));
 
-$qArt='SELECT * FROM eurolist ORDER BY art';
+$qArt='SELECT art, dyntaxa_id, arthela, latin, rank FROM eurolist ORDER BY art';
 $rArt = pg_query($db_connection, $qArt);
 while ($rowArt = pg_fetch_array($rArt)) {
   $tabArtRef[]=$rowArt;
 }
+$consoleTxt.=consoleMessage("info", count($tabArtRef)." element(s) in eurolist");
 
 $tabRecap=array();
 
@@ -26,26 +27,47 @@ foreach ($tabArtRef as $rowArt) {
 
     if (isset($oneList[$rowArt["art"]])) {
 
-      /*if (!isset($tabRecap[$rowArt["art"]])) {
-        $tabRecap[$rowArt["art"]]=array();
-
-        $tabRecap[$rowArt["art"]]["eurolist"]=array(
-          "dyntaxa_id" => $rowArt["dyntaxa_id"],
-          "latin" => $rowArt["latin"]
-        );
-      }
-
-      $tabRecap[$rowArt["art"]][$listId]=array(
-        "dyntaxa_id" => $oneList[$rowArt["art"]]["dyntaxaId"],
-        "latin" => $oneList[$rowArt["art"]]["name"]
-      );*/
+      $errorsRow=array();
 
       if ($rowArt["dyntaxa_id"]!=$oneList[$rowArt["art"]]["dyntaxaId"]) {
         $consoleTxt.=consoleMessage("error", "Different dyntaxaId for art ".$rowArt["art"]." in ".$listId." : ".$rowArt["dyntaxa_id"]."/".$oneList[$rowArt["art"]]["dyntaxaId"]);
+        $errorsRow[]="dyntaxa_id";
       }
       if ($rowArt["latin"]!=$oneList[$rowArt["art"]]["name"]) {
         $consoleTxt.=consoleMessage("error", "Different scientificName for art ".$rowArt["art"]." in ".$listId." : ".$rowArt["latin"]."/".$oneList[$rowArt["art"]]["name"]);
+        $errorsRow[]="latin";
       }
+      if ($rowArt["arthela"]!=$oneList[$rowArt["art"]]["nameSWE"]) {
+        $consoleTxt.=consoleMessage("error", "Different swedishName for art ".$rowArt["art"]." in ".$listId." : ".$rowArt["arthela"]."/".$oneList[$rowArt["art"]]["nameSWE"]);
+        $errorsRow[]="arthela";
+      }
+      if ($rowArt["rank"]!=$oneList[$rowArt["art"]]["rank"]) {
+        $consoleTxt.=consoleMessage("error", "Different rank for art ".$rowArt["art"]." in ".$listId." : ".$rowArt["rank"]."/".$oneList[$rowArt["art"]]["rank"]);
+        $errorsRow[]="rank";
+      }
+
+      if (count($errorsRow)>0) {
+        if (!isset($tabRecap[$rowArt["art"]])) {
+          $tabRecap[$rowArt["art"]]=array();
+
+          $tabRecap[$rowArt["art"]]["eurolist"]=array(
+            "dyntaxa_id" => $rowArt["dyntaxa_id"],
+            "latin" => $rowArt["latin"],
+            "arthela" => $rowArt["arthela"],
+            "rank" => $rowArt["rank"],
+            "errors" => []
+          );
+        }
+
+        $tabRecap[$rowArt["art"]][$listId]=array(
+          "dyntaxa_id" => $oneList[$rowArt["art"]]["dyntaxaId"],
+          "latin" => $oneList[$rowArt["art"]]["name"],
+          "arthela" => $oneList[$rowArt["art"]]["nameSWE"],
+          "rank" => $oneList[$rowArt["art"]]["rank"],
+          "errors" => $errorsRow
+        );
+      }
+
     }
   }
 
--- views/list_sync_compare.php
+++ views/list_sync_compare.php
@@ -1,3 +1,13 @@
+<!-- bootstrap tables -->
+<link href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css" rel="stylesheet">
+<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
+<!-- bootstrap tables + export buttons -->
+<script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/tableExport.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/libs/jsPDF/jspdf.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/libs/jsPDF-AutoTable/jspdf.plugin.autotable.js"></script>
+<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/extensions/export/bootstrap-table-export.min.js"></script>
+
+
 <div class="container">
 
   <div class="float-right">Hi <?= $current_user->display_name ?> ! <a href="<?= URL_LOGOUT ?>">logout</a></div><br>
@@ -7,7 +17,7 @@
   <p class="lead">
   	Getting lists data from <a href="https://lists.biodiversitydata.se" target="_blank">lists.biodiversitydata.se</a> and compare with what exists right now in the STF database, table eurolist.
   </p>
-
+  <p>The recap array displays both the field from Eurolist, and then from the list</p>
   <form role="form" method="post">
   	<input type="hidden" value="OK" id="form" name="formCompareLists"/>
 
@@ -36,6 +46,63 @@
     <b><?= $final_result ?></b>
   </p>
   <?php } ?>
+
+  <?php if (count($tabRecap)>0) { ?>
+
+    <div class="form-group row">
+      
+      <table id="table"
+        data-toggle="table"
+        data-show-columns="true"
+        data-search="true"
+        data-show-export="true">
+        <thead>
+          <tr>
+            <th>art</th>
+            <th>dyntaxaId</th>
+            <th>scientificName</th>
+            <th>swedishName</th>
+            <th>rank</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($tabRecap as $art => $rowContent) { ?>
+            <tr>
+              <th><?= $art ?>/eurolist</th>
+              <td scope="row"><?= $rowContent["eurolist"]["dyntaxa_id"] ?></td>
+              <td scope="row"><?= $rowContent["eurolist"]["latin"] ?></td>
+              <td scope="row"><?= $rowContent["eurolist"]["arthela"] ?></td>
+              <td scope="row"><?= $rowContent["eurolist"]["rank"] ?></td>
+            </tr>
+            <?php foreach ($rowContent as $listId => $listCont) { 
+              if ($listId!="eurolist") { ?>
+              <tr>
+                <th><?= $art."/".$listId ?></th>
+                <?php if (in_array("dyntaxa_id", $listCont["errors"])) $bckgdColor="#FF6347"; else $bckgdColor=""; ?>
+                <td scope="row" <?= ($bckgdColor!="" ? 'style="background-color:'.$bckgdColor.'"' : "") ?>><?= $listCont["dyntaxa_id"] ?></td>
+                <?php if (in_array("latin", $listCont["errors"])) $bckgdColor="#FF6347"; else $bckgdColor=""; ?>
+                <td scope="row" <?= ($bckgdColor!="" ? 'style="background-color:'.$bckgdColor.'"' : "") ?>><?= $listCont["latin"] ?></td>
+                <?php if (in_array("arthela", $listCont["errors"])) $bckgdColor="#FF6347"; else $bckgdColor=""; ?>
+                <td scope="row" <?= ($bckgdColor!="" ? 'style="background-color:'.$bckgdColor.'"' : "") ?>><?= $listCont["arthela"] ?></td>
+                <?php if (in_array("rank", $listCont["errors"])) $bckgdColor="#FF6347"; else $bckgdColor=""; ?>
+                <td scope="row" <?= ($bckgdColor!="" ? 'style="background-color:'.$bckgdColor.'"' : "") ?>><?= $listCont["rank"] ?></td>
+              </tr>
+            <?php }} ?>
+          <?php } ?>
+        </tbody>
+      </table>
+
+      <script>
+        $(function() {
+          $('#table').bootstrapTable()
+        })
+      </script>
+
+
+    </div>
+  <?php }  ?>
+
+
 
   <div class="form-group row">
     <label for="consoleArea" class="col-sm-2 col-form-label">CONSOLE</label>
