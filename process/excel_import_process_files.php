de6cfe2c77a8185b5d86bbb57b8bc87935f97070
mathieuLU <you@example.com>
2023-03-20 14:53:18 +0100

    add extra minutes for finishtime

--- process/excel_import_process_files.php
+++ process/excel_import_process_files.php
@@ -3,6 +3,26 @@
 use MongoDB\BSON\UTCDateTime;
 
 
+function addTimeToFinish($finish_time, $minutesToAdd) {
+
+	// add minutes
+	$finish_time_extramin=str_pad($finish_time, 4, '0', STR_PAD_LEFT);
+	$hours=intval(substr($finish_time_extramin, 0, 2));
+	$minutes=intval(substr($finish_time_extramin, 2, 2));
+	if ($minutes>=(60 - $minutesToAdd)) {
+		$minutes=str_pad(($minutes+$minutesToAdd)-60, 2, '0', STR_PAD_LEFT);
+
+		if ($hours==23) $hours=0;
+		else $hours++;
+	}
+	else {
+		$minutes+=$minutesToAdd;
+	}
+	$finish_time=intval($hours.$minutes);
+
+	return $finish_time;
+}
+
 // 4** PROCESS FOR OK FILES
 
 switch($protocol) {
@@ -250,20 +270,8 @@ foreach($listFilesOk as $file) {
 				$start_time_brut=$start_time;
 
 
-				// add 5 minutes 
-				$finish_time_5min=str_pad($finish_time, 4, '0', STR_PAD_LEFT);
-				$hours=intval(substr($finish_time_5min, 0, 2));
-				$minutes=intval(substr($finish_time_5min, 2, 2));
-				if ($minutes>=55) {
-					$minutes=str_pad(($minutes+5)-60, 2, '0', STR_PAD_LEFT);
-
-					if ($hours==23) $hours=0;
-					else $hours++;
-				}
-				else {
-					$minutes+=5;
-				}
-				$finish_time=intval($hours.$minutes);
+				// add extra minutes (natt : 5)
+				$finish_time=addTimeToFinish($finish_time, 5);
 
 				$rowDisturbance=146;
 				$disturbances=array();
@@ -390,21 +398,8 @@ foreach($listFilesOk as $file) {
 
 				$start_time_brut=$start_time;
 
-
-				// add 5 minutes 
-				$finish_time_5min=str_pad($finish_time, 4, '0', STR_PAD_LEFT);
-				$hours=intval(substr($finish_time_5min, 0, 2));
-				$minutes=intval(substr($finish_time_5min, 2, 2));
-				if ($minutes>=55) {
-					$minutes=str_pad(($minutes+5)-60, 2, '0', STR_PAD_LEFT);
-
-					if ($hours==23) $hours=0;
-					else $hours++;
-				}
-				else {
-					$minutes+=5;
-				}
-				$finish_time=intval($hours.$minutes);
+				// add extra minutes (std: 35)
+				$finish_time=addTimeToFinish($finish_time, 35);
 
 				$minutesSpentObserving=array();
 				$iInd=1;
