db37d98eeee87a5289de1a1230c3c133a6e698aa
Mathieu Blanchet <mathieu.blanchet@biol.lu.se>
2024-01-31 16:47:33 +0100

    modify layout for point coordinates and import

--- views/coordinates_site_punkt.php
+++ views/coordinates_site_punkt.php
@@ -12,32 +12,15 @@
 
   <div class="float-right">Hi <?= $current_user->display_name ?> ! <a href="<?= URL_LOGOUT ?>">logout</a></div><br>
 
-  <h2>Punktrutter coordinates list and import</h2>
+  <h2>Punktrutter coordinates list</h2>
 
   <p class="lead">
-  	Listing/importing coordinates for punktrutterna from/in Ecodata/BioCollect
+  	Listing coordinates for punktrutterna from Ecodata/BioCollect
   </p>
   <?php  if ($modeDisplay=="coord") { ?>
     <p><a href="?display=default"><input type="button" value="Show all the sites" name="submit" class="btn btn-primary"/></a></p>
   <?php } else { ?>
     <p><a href="?display=coord"><input type="button" value="Show all the coordinates" name="submit" class="btn btn-primary"/></a></p>
-  <?php } ?>
-
-  <form role="form" method="post">
-    <label>Import new coordinates from an excel file following the template :</label>
-  	<input type="hidden" value="OK" id="formCoordPunktSite" name="formCoordPunktSite"/>
-
-    <div class="form-group row">
-      <div class="offset-sm-2 col-sm-10">
-        <input type="submit" value="Import" name="submit" class="btn btn-primary"/>
-      </div>
-    </div>
-  </form>
-
-  <?php if (isset($final_result) &&  $final_result!="") { ?>
-  <p class="lead">
-    <b><?= $final_result ?></b>
-  </p>
   <?php } ?>
 
   <?php if (isset($arrCoordSites) && count($arrCoordSites)>0) { ?>
