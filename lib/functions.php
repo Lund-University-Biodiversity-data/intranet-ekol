cdc8c6cf772dbac68b0b8821ff9f3348571f09fe
Mathieu Blanchet <mathieu.blanchet@biol.lu.se>
2024-04-09 17:20:25 +0200

    add functions

---
+++ lib/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+function getNpPtsForProtocol ($protocol) {
+	
+
+	switch($protocol) {
+		case "std":
+			$nbPts=8;
+			break;
+		case "sommar":
+		case "vinter":
+			$nbPts=20;
+			break;
+		case "natt":
+			$nbPts=20;
+			break;
+		case "kust":
+			$nbPts=1;
+			break;
+		default:
+			$nbPts=0;
+			break;
+	}
+
+	return $nbPts;
+}
+
+
+
+?>
